2ac38ad0e081840982d412c113c7b36a4fc17bde
Joshua Larsen <lar21022@byui.edu>
2022-05-19 14:53:27 -0600

    Finilized Stedi site

--- CIT71repostory/manageuser.js
+++ CIT71repostory/manageuser.js
@@ -1,21 +1,18 @@
 //© 2021 Sean Murdock
-let phonenumber = "";
+let phoneNumber = "";
 let userName = "";
 let password = "";
 let verifypassword = "";
 let passwordRegEx=/((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%!]).{6,40})/;
 
 function setphonenumber(){
-phonenumber = $("#phonenumber").val();
+phoneNumber = $("#phonenumber").val();
 
 }
 
 function setuserpassword(){
     password = $("#password").val();
-    var valid=passwordRegEx.exec(password);
-    if (!valid){
-        alert('Must be 6 digits, upper, lower, number, and symbol');
-    }
+   
 }
 
 function setverifypassword(){
@@ -50,7 +47,7 @@ function checkexpiredtoken(token){
 function sendtext(){
     $.ajax({
         type: 'POST',
-        url: 'https://dev.stedi.me/twofactorlogin/'+phonenumber,
+        url: 'https://dev.stedi.me/twofactorlogin/'+phoneNumber,
         contentType: 'application/text',
         dataType: 'text'
     }
@@ -59,12 +56,12 @@ function sendtext(){
 }
 
 function userlogin(){
-    setuserpassword();
+
 
     $.ajax({
         type: 'POST',
-        url: 'https://dev.stedi.me/login',
-        data: JSON.stringify({userName, password}),
+        url: 'https://dev.stedi.me/twofactorlogin',
+        data: JSON.stringify({phoneNumber, oneTimePassword:password}),
         success: function(data) {
             window.location.href = "/timer.html#"+data;//add the token to the url
         },
